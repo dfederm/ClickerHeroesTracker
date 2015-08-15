d3d825822818f44eeb9b95e904bb44ac6935496d
David Federman <david.federman@outlook.com>
2015-08-15 01:51:37 +0000

    Loosen password requirements

--- ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/App_Start/IdentityConfig.cs
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/App_Start/IdentityConfig.cs
@@ -36,11 +36,7 @@ namespace ClickerHeroesTrackerWebsite
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 4,
             };
 
             // Configure user lockout defaults
