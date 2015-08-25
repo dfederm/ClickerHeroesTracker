8ccaf643bdeef491c1174da23e7a5ee12a2fbcc4
David Federman <david.federman@outlook.com>
2015-08-25 05:48:02 +0000

    Added Hybrid play style to the suggested ancient level plan

--- ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Models/Calculator/SuggestedAncientLevelsViewModel.cs
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Models/Calculator/SuggestedAncientLevelsViewModel.cs
@@ -6,6 +6,18 @@
 
     public class SuggestedAncientLevelsViewModel
     {
+        private static ISet<PlayStyle> allPlayStyles = new HashSet<PlayStyle>(new[]
+        {
+            PlayStyle.Idle,
+            PlayStyle.Hybrid,
+            PlayStyle.Active,
+        });
+
+        private static ISet<PlayStyle> hybridPlayStyles = new HashSet<PlayStyle>(new[]
+        {
+            PlayStyle.Hybrid,
+        });
+
         public SuggestedAncientLevelsViewModel(
             IDictionary<Ancient, int> ancientLevels,
             int optimalLevel,
@@ -19,15 +31,19 @@
             var currentLiberLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Libertas);
             var currentMammonLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Mammon);
             var currentMimzeeLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Mimzee);
+            var currentFragsworthLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Fragsworth);
+            var currentBhaalLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Bhaal);
+            var currentPlutoLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Pluto);
+            var currentJuggernautLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Juggernaut);
             var currentIrisLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Iris);
             var currentSolomonLevel = GetCurrentAncientLevel(ancientLevels, Ancient.Solomon);
 
             var suggestedSiyaLevel = currentSiyaLevel;
             var suggestedArgaivLevel = suggestedSiyaLevel + 9;
             var suggestedMorgLevel = (int)Math.Round(Math.Pow(suggestedSiyaLevel, 2) + (43.67 * suggestedSiyaLevel) + 33.58);
-            var suggestedLiberLevel = (int)Math.Round(suggestedSiyaLevel * 0.93);
-            var suggestedMammonLevel = suggestedLiberLevel;
-            var suggestedMimzeeLevel = suggestedLiberLevel;
+            var suggestedGoldLevel = (int)Math.Round(suggestedSiyaLevel * 0.93);
+            var suggestedClickLevel = (int)Math.Round(suggestedSiyaLevel * 0.5);
+            var suggestedJuggernautLevel = (int)Math.Round(suggestedClickLevel * 0.2);
             var suggestedIrisLevel = optimalLevel - 1001;
             var suggestedSolomonLevel = userSettings.UseReducedSolomonFormula
                 ? (int)Math.Round(1.15 * Math.Pow(Math.Log10(3.25 * Math.Pow(suggestedSiyaLevel, 2)), 0.4) * Math.Pow(suggestedSiyaLevel, 0.8))
@@ -35,14 +51,18 @@
 
             this.SuggestedAncientLevels = new SuggestedAncientLevelData[]
             {
-                new SuggestedAncientLevelData(Ancient.Siyalatas, currentSiyaLevel, suggestedSiyaLevel),
-                new SuggestedAncientLevelData(Ancient.Argaiv, currentArgaivLevel, suggestedArgaivLevel),
-                new SuggestedAncientLevelData(Ancient.Morgulis, currentMorgLevel, suggestedMorgLevel),
-                new SuggestedAncientLevelData(Ancient.Libertas, currentLiberLevel, suggestedLiberLevel),
-                new SuggestedAncientLevelData(Ancient.Mammon, currentMammonLevel, suggestedMammonLevel),
-                new SuggestedAncientLevelData(Ancient.Mimzee, currentMimzeeLevel, suggestedMimzeeLevel),
-                new SuggestedAncientLevelData(Ancient.Iris, currentIrisLevel, suggestedIrisLevel),
-                new SuggestedAncientLevelData(Ancient.Solomon, currentSolomonLevel, suggestedSolomonLevel),
+                new SuggestedAncientLevelData(Ancient.Siyalatas, currentSiyaLevel, suggestedSiyaLevel, allPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Argaiv, currentArgaivLevel, suggestedArgaivLevel, allPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Morgulis, currentMorgLevel, suggestedMorgLevel, allPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Libertas, currentLiberLevel, suggestedGoldLevel, allPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Mammon, currentMammonLevel, suggestedGoldLevel, allPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Mimzee, currentMimzeeLevel, suggestedGoldLevel, allPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Fragsworth, currentFragsworthLevel, suggestedClickLevel, hybridPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Bhaal, currentBhaalLevel, suggestedClickLevel, hybridPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Pluto, currentPlutoLevel, suggestedClickLevel, hybridPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Juggernaut, currentJuggernautLevel, suggestedJuggernautLevel, hybridPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Iris, currentIrisLevel, suggestedIrisLevel, allPlayStyles),
+                new SuggestedAncientLevelData(Ancient.Solomon, currentSolomonLevel, suggestedSolomonLevel, allPlayStyles),
             };
         }
 
@@ -60,7 +80,11 @@
 
         public class SuggestedAncientLevelData
         {
-            public SuggestedAncientLevelData(Ancient ancient, int currentLevel, int suggestedLevel)
+            public SuggestedAncientLevelData(
+                Ancient ancient,
+                int currentLevel,
+                int suggestedLevel,
+                ISet<PlayStyle> supportedPlayStyles)
             {
                 suggestedLevel = Math.Max(suggestedLevel, 0);
 
@@ -68,6 +92,7 @@
                 this.CurrentLevel = currentLevel.ToString();
                 this.SuggestedLevel = suggestedLevel.ToString();
                 this.LevelDifference = (suggestedLevel - currentLevel).ToString();
+                this.SupportedPlayStyles = supportedPlayStyles;
             }
 
             public string AncientName { get; private set; }
@@ -77,6 +102,8 @@
             public string SuggestedLevel { get; private set; }
 
             public string LevelDifference { get; private set; }
+
+            public ISet<PlayStyle> SupportedPlayStyles { get; private set; }
         }
     }
 }
