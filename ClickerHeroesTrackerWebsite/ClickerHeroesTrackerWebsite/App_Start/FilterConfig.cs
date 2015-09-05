99042a994a6e5a8a81122130b7615d85a7632326
David Federman <david.federman@outlook.com>
2015-09-05 17:02:11 +0000

    Add per-request instrumentation for User and Referrer

--- ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/App_Start/FilterConfig.cs
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleAndInstrumentErrorFilter());
+            filters.Add(new UserInstrumentationFilter());
         }
     }
 }
---
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Filters/UserInstrumentationFilter.cs
@@ -0,0 +1,36 @@
+﻿namespace ClickerHeroesTrackerWebsite.Filters
+{
+    using Microsoft.ApplicationInsights;
+    using Microsoft.AspNet.Identity;
+    using System.Collections.Generic;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class UserInstrumentationFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var user = filterContext.HttpContext.User.Identity;
+
+            var telemetry = new TelemetryClient();
+            telemetry.TrackEvent(
+                "Request",
+                new Dictionary<string, string>
+                {
+                    { "UserId", user.GetUserId() ?? "<anonymous>" },
+                    { "UserName", user.GetUserName() ?? "<anonymous>" },
+                    { "Referrer", GetReferrer(filterContext.HttpContext.Request) },
+                });
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+        }
+
+        private static string GetReferrer(HttpRequestBase request)
+        {
+            var referrer = request.UrlReferrer;
+            return referrer != null ? referrer.OriginalString : "<none>";
+        }
+    }
+}
