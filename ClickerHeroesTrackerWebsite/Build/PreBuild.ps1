ed6e5c34cea13dddf20c66a329942dfdb966d040
David Federman <david.federman@outlook.com>
2015-11-15 07:43:34 +0000

    Fix error in Prebuild

--- ClickerHeroesTrackerWebsite/Build/PreBuild.ps1
+++ ClickerHeroesTrackerWebsite/Build/PreBuild.ps1
@@ -12,18 +12,20 @@ try
 
 	Write-Host "Using version $SourceVersion"
  
-	$assemblyInfoFile = Get-ChildItem -Path $SourceDirectory -Filter "AssemblyInfo.cs" -Recurse
-	Write-Host "Changing $assemblyInfoFile"
+	$assemblyInfoFiles = Get-ChildItem -Path $SourceDirectory -Filter "AssemblyInfo.cs" -Recurse | %{
+		$assemblyInfoFile = $_.FullName
+		Write-Host "Changing $assemblyInfoFile"
          
-	# Remove the read-only bit on the file
-	Set-ItemProperty -Path $assemblyInfoFile -Name IsReadOnly -Value $false
+		# Remove the read-only bit on the file
+		Set-ItemProperty -Path $assemblyInfoFile -Name IsReadOnly -Value $false
  
-	# Replace the version number
-	$content = Get-Content -Path $assemblyInfoFile
-	$content = $content -Replace $versionPattern,"$1.$2.$SourceVersion.$4"
+		# Replace the version number
+		$content = Get-Content -Path $assemblyInfoFile
+		$content = $content -Replace $versionPattern,"$1.$2.$SourceVersion.$4"
 
-	# Re-write the file
-	Set-Content -Path $assemblyInfoFile -Value $content
+		# Re-write the file
+		Set-Content -Path $assemblyInfoFile -Value $content
+    }
  
 	Write-Host "Done!"
 }
