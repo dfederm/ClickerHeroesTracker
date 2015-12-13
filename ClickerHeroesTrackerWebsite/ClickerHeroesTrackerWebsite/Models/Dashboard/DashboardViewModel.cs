287a2aa5aabd3f075e5212aa0c5e86b19de808e4
David Federman <david.federman@outlook.com>
2015-12-13 17:42:23 +0000

    Fix exception on the dashboard when the user has no uploads

--- ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Models/Dashboard/DashboardViewModel.cs
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Models/Dashboard/DashboardViewModel.cs
@@ -48,75 +48,77 @@ namespace ClickerHeroesTrackerWebsite.Models.Dashboard
                 data = new ProgressData(reader, userSettings);
             }
 
-            if (data.SoulsPerHourData.Count > 0)
+            if (!data.IsValid || data.SoulsPerHourData.Count == 0)
             {
-                this.ProgressSummaryGraph = new GraphData
-                {
-                    Chart = new Chart
-                    {
-                        Type = ChartType.Line
-                    },
-                    Title = new Title
-                    {
-                        Text = "Souls/hr"
-                    },
-                    XAxis = new Axis
-                    {
-                        TickInterval = 24 * 3600 * 1000, // one day
-                        Type = AxisType.Datetime,
-                        TickWidth = 0,
-                        GridLineWidth = 1,
-                        Labels = new Labels
-                        {
-                            Align = Align.Left,
-                            X = 3,
-                            Y = -3,
-                            Format = "{value:%m/%d}"
-                        }
-                    },
-                    YAxis = new Axis
-                    {
-                        Labels = new Labels
-                        {
-                            Align = Align.Left,
-                            X = 3,
-                            Y = 16,
-                            Format = "{value:.,0f}"
-                        },
-                        ShowFirstLabel = false
-                    },
-                    Legend = new Legend
-                    {
-                        Enabled = false
-                    },
-                    Series = new Series[]
-                    {
-                        new Series
-                        {
-                            Data = data
-                                .SoulsPerHourData
-                                .Select(datum => new Point
-                                {
-                                    X = datum.Key.ToJavascriptTime(userSettings.TimeZone),
-                                    Y = datum.Value
-                                })
-                                .Concat(new[]
-                                {
-                                    new Point
-                                    {
-                                        X = DateTime.UtcNow.ToJavascriptTime(userSettings.TimeZone),
-                                        Y = data.SoulsPerHourData.Last().Value
-                                    }
-                                })
-                                .ToList()
-                        }
-                    }
-                };
+                return;
             }
+
+            this.ProgressSummaryGraph = new GraphData
+            {
+                Chart = new Chart
+                {
+                    Type = ChartType.Line
+                },
+                Title = new Title
+                {
+                    Text = "Souls/hr"
+                },
+                XAxis = new Axis
+                {
+                    TickInterval = 24 * 3600 * 1000, // one day
+                    Type = AxisType.Datetime,
+                    TickWidth = 0,
+                    GridLineWidth = 1,
+                    Labels = new Labels
+                    {
+                        Align = Align.Left,
+                        X = 3,
+                        Y = -3,
+                        Format = "{value:%m/%d}"
+                    }
+                },
+                YAxis = new Axis
+                {
+                    Labels = new Labels
+                    {
+                        Align = Align.Left,
+                        X = 3,
+                        Y = 16,
+                        Format = "{value:.,0f}"
+                    },
+                    ShowFirstLabel = false
+                },
+                Legend = new Legend
+                {
+                    Enabled = false
+                },
+                Series = new Series[]
+                {
+                    new Series
+                    {
+                        Data = data
+                            .SoulsPerHourData
+                            .Select(datum => new Point
+                            {
+                                X = datum.Key.ToJavascriptTime(userSettings.TimeZone),
+                                Y = datum.Value
+                            })
+                            .Concat(new[]
+                            {
+                                new Point
+                                {
+                                    X = DateTime.UtcNow.ToJavascriptTime(userSettings.TimeZone),
+                                    Y = data.SoulsPerHourData.Last().Value
+                                }
+                            })
+                            .ToList()
+                    }
+                }
+            };
 
             this.RivalDataList = new RivalDataList(databaseCommandFactory, userId);
 
-            this.IsValid = data.IsValid;
+            this.IsValid = true;
         }
 
         /// <summary>
