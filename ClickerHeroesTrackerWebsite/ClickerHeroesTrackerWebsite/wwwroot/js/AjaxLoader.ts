0087e909e660e14b6fd670e2c9886ca0af3787aa
Daniel Wood <daniel.wood@rxpservices.com>
2016-07-12 01:21:30 +0000

    PR 87: Ajax loading spinner
     - Added cog next to username
     - Revert "Added cog next to username"
     - Merge branch 'master' of
     - Code for handling ajax loading spinner and error alerts.
     - Added comments
    
    Related work items: #51

---
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/wwwroot/js/AjaxLoader.ts
@@ -0,0 +1,15 @@
+﻿namespace AjaxLoader {
+    "use strict";
+
+    // Get the loading div as variable
+    const loadingElement = $("#loadingElement").hide();
+
+    // Show and hide loading spinner on Ajax start/stop.
+    $(document)
+        .ajaxStart(function(): void {
+            loadingElement.show();
+        })
+        .ajaxStop(function(): void {
+            loadingElement.hide();
+        });
+}
