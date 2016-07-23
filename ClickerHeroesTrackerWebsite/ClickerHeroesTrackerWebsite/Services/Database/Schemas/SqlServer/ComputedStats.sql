5648ee597004ea7b73839790c72d472a8491472f
David Federman <david.federman@outlook.com>
2016-07-23 16:25:31 +0000

    PR 110: Merge dfederm/larger-numbers to master
     - Support larger numbers in the database

--- ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Services/Database/Schemas/SqlServer/ComputedStats.sql
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Services/Database/Schemas/SqlServer/ComputedStats.sql
@@ -1,23 +1,26 @@
 ﻿CREATE TABLE [dbo].[ComputedStats] (
-    [UploadId]                         INT      NOT NULL,
-    [OptimalLevel]                     SMALLINT DEFAULT ((0)) NOT NULL,
-    [SoulsPerHour]                     BIGINT   DEFAULT ((0)) NOT NULL,
-    [SoulsPerAscension]                BIGINT   DEFAULT ((0)) NOT NULL,
-    [AscensionTime]                    SMALLINT DEFAULT ((0)) NOT NULL,
-    [TitanDamage]                      REAL     DEFAULT ((0)) NOT NULL,
-    [SoulsSpent]                       REAL     DEFAULT ((0)) NOT NULL,
-    [HeroSoulsSacrificed]              REAL     DEFAULT ((0)) NOT NULL,
-    [TotalAncientSouls]                REAL     DEFAULT ((0)) NOT NULL,
-    [TranscendentPower]                REAL     DEFAULT ((0)) NOT NULL,
-    [Rubies]                           REAL     DEFAULT ((0)) NOT NULL,
-    [HighestZoneThisTranscension]      REAL     DEFAULT ((0)) NOT NULL,
-    [HighestZoneLifetime]              REAL     DEFAULT ((0)) NOT NULL,
-    [AscensionsThisTranscension]       REAL     DEFAULT ((0)) NOT NULL,
-    [AscensionsLifetime]               REAL     DEFAULT ((0)) NOT NULL,
-    [MaxTranscendentPrimalReward]      REAL     DEFAULT ((0)) NOT NULL,
-    [BossLevelToTranscendentPrimalCap] REAL     DEFAULT ((0)) NOT NULL,
+    [UploadId]                         INT        NOT NULL,
+    [OptimalLevel]                     SMALLINT   DEFAULT ((0)) NOT NULL,
+    [SoulsPerHour]                     BIGINT     DEFAULT ((0)) NOT NULL,
+    [SoulsPerAscension]                BIGINT     DEFAULT ((0)) NOT NULL,
+    [AscensionTime]                    SMALLINT   DEFAULT ((0)) NOT NULL,
+    [TitanDamage]                      FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [SoulsSpent]                       FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [HeroSoulsSacrificed]              FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [TotalAncientSouls]                FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [TranscendentPower]                FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [Rubies]                           FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [HighestZoneThisTranscension]      FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [HighestZoneLifetime]              FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [AscensionsThisTranscension]       FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [AscensionsLifetime]               FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [MaxTranscendentPrimalReward]      FLOAT (53) DEFAULT ((0)) NOT NULL,
+    [BossLevelToTranscendentPrimalCap] FLOAT (53) DEFAULT ((0)) NOT NULL,
     CONSTRAINT [FK_ComputedStats_ToUpload] FOREIGN KEY ([UploadId]) REFERENCES [dbo].[Uploads] ([Id])
 );
 
+
+GO
 CREATE CLUSTERED INDEX [UploadIdIndex]
     ON [dbo].[ComputedStats]([UploadId] ASC);
+
