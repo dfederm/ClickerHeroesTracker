842add6abe705319ca45e6a5f0e80fa2d6b73559
David Federman <david.federman@outlook.com>
2016-07-29 04:30:32 +0000

    PR 128: Fix the error page
     - Fix the error page
    
    Related work items: #140

--- ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Startup.cs
+++ ClickerHeroesTrackerWebsite/ClickerHeroesTrackerWebsite/Startup.cs
@@ -32,7 +32,7 @@ namespace ClickerHeroesTrackerWebsite
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
             }
 
             // Exception middleware should be added after error page and any other error handling middleware.
