7f5e011e540f9ddb08e362776da95c8af3bc9244
David Federman <david.federman@outlook.com>
2017-05-05 22:59:39 -0700

    Add more component tests (#77)

---
+++ src/Website/wwwroot/js/components/home/home.spec.ts
@@ -0,0 +1,58 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+import { HomeComponent } from "./home";
+
+describe("HomeComponent", () =>
+{
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
+
+    beforeEach(async(() =>
+    {
+        TestBed.configureTestingModule(
+        {
+            declarations: [ HomeComponent ],
+            schemas:      [ NO_ERRORS_SCHEMA ],
+        })
+        .compileComponents()
+        .then(() =>
+        {
+            fixture = TestBed.createComponent(HomeComponent);
+            component = fixture.componentInstance;
+        });
+    }));
+
+    it("should display the jumbotron", () =>
+    {
+        fixture.detectChanges();
+
+        let jumbotron = fixture.debugElement.query(By.css(".jumbotron"));
+        expect(jumbotron).not.toBeNull();
+
+        let title = jumbotron.query(By.css("h1"));
+        expect(title).not.toBeNull();
+        expect(title.nativeElement.textContent).toEqual("Clicker Heroes Tracker");
+
+        let uploadLink = jumbotron.query(By.css("a"));
+        expect(uploadLink).not.toBeNull();
+        expect(uploadLink.attributes["href"]).toEqual("/Upload");
+    });
+
+    it("should display the short changelog", () =>
+    {
+        fixture.detectChanges();
+
+        let changelogContainer = fixture.debugElement.query(By.css(".col-md-4"));
+        expect(changelogContainer).not.toBeNull();
+
+        let changelog = changelogContainer.query(By.css("changelog"));
+        expect(changelog).not.toBeNull();
+        expect(changelog.properties["isFull"]).toEqual(false);
+
+        let fullChangelogLink = changelogContainer.query(By.css("a"));
+        expect(fullChangelogLink).not.toBeNull();
+        expect(fullChangelogLink.attributes["routerLink"]).toEqual("/news");
+    });
+});
---
+++ src/Website/wwwroot/js/components/news/news.spec.ts
@@ -0,0 +1,38 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+import { NewsComponent } from "./news";
+
+describe("NewsComponent", () =>
+{
+    let component: NewsComponent;
+    let fixture: ComponentFixture<NewsComponent>;
+
+    beforeEach(async(() =>
+    {
+        TestBed.configureTestingModule(
+        {
+            declarations: [ NewsComponent ],
+            schemas:      [ NO_ERRORS_SCHEMA ],
+        })
+        .compileComponents()
+        .then(() =>
+        {
+            fixture = TestBed.createComponent(NewsComponent);
+            component = fixture.componentInstance;
+        });
+    }));
+
+    it("should display the full changelog", () =>
+    {
+        fixture.detectChanges();
+
+        let changelogContainer = fixture.debugElement.query(By.css(".col-md-6"));
+        expect(changelogContainer).not.toBeNull();
+
+        let changelog = changelogContainer.query(By.css("changelog"));
+        expect(changelog).not.toBeNull();
+        expect(changelog.properties["isFull"]).toEqual(true);
+    });
+});
