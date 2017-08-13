4aa39ec3afd0397a8a8b5326b2bf6cc424940797
David Federman <david.federman@outlook.com>
2017-08-12 21:27:00 -0700

    Implement token refresh

--- src/Website/Controllers/Api/AuthenticationController.cs
+++ src/Website/Controllers/Api/AuthenticationController.cs
@@ -5,20 +5,30 @@
 namespace ClickerHeroesTrackerWebsite.Controllers.Api
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using AspNet.Security.OpenIdConnect.Extensions;
     using AspNet.Security.OpenIdConnect.Primitives;
     using AspNet.Security.OpenIdConnect.Server;
     using ClickerHeroesTrackerWebsite.Models;
     using Microsoft.AspNetCore.Authentication;
-    using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Options;
+    using OpenIddict.Core;
 
     [Route("api/auth")]
     public class AuthenticationController : Controller
     {
+        private static readonly string[] AllowedRefreshTokenScopes = new[]
+        {
+            OpenIdConnectConstants.Scopes.OpenId,
+            OpenIdConnectConstants.Scopes.Email,
+            OpenIdConnectConstants.Scopes.Profile,
+            OpenIdConnectConstants.Scopes.OfflineAccess,
+            OpenIddictConstants.Scopes.Roles,
+        };
+
         private readonly IOptions<IdentityOptions> identityOptions;
         private readonly SignInManager<ApplicationUser> signInManager;
         private readonly UserManager<ApplicationUser> userManager;
@@ -65,6 +75,38 @@ namespace ClickerHeroesTrackerWebsite.Controllers.Api
 
                 return this.SignIn(ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
             }
+            else if (request.IsRefreshTokenGrantType())
+            {
+                // Retrieve the claims principal stored in the refresh token.
+                var result = await this.HttpContext.AuthenticateAsync(OpenIdConnectServerDefaults.AuthenticationScheme);
+
+                // Retrieve the user profile corresponding to the refresh token.
+                var user = await this.signInManager.ValidateSecurityStampAsync(result.Principal);
+                if (user == null)
+                {
+                    return this.BadRequest(new OpenIdConnectResponse
+                    {
+                        Error = OpenIdConnectConstants.Errors.InvalidGrant,
+                        ErrorDescription = "The refresh token is no longer valid.",
+                    });
+                }
+
+                // Ensure the user is still allowed to sign in.
+                if (!await this.signInManager.CanSignInAsync(user))
+                {
+                    return this.BadRequest(new OpenIdConnectResponse
+                    {
+                        Error = OpenIdConnectConstants.Errors.InvalidGrant,
+                        ErrorDescription = "The user is no longer allowed to sign in.",
+                    });
+                }
+
+                // Create a new authentication ticket, but reuse the properties stored
+                // in the refresh token, including the scopes originally granted.
+                var ticket = await this.CreateTicketAsync(request, user, result.Properties);
+
+                return this.SignIn(ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
+            }
 
             return this.BadRequest(new OpenIdConnectResponse
             {
@@ -73,17 +115,22 @@ namespace ClickerHeroesTrackerWebsite.Controllers.Api
             });
         }
 
-        private async Task<AuthenticationTicket> CreateTicketAsync(OpenIdConnectRequest request, ApplicationUser user)
+        private async Task<AuthenticationTicket> CreateTicketAsync(OpenIdConnectRequest request, ApplicationUser user, AuthenticationProperties properties = null)
         {
             // Create a new ClaimsPrincipal containing the claims that
             // will be used to create an id_token, a token or a code.
             var principal = await this.signInManager.CreateUserPrincipalAsync(user);
 
             // Create a new authentication ticket holding the user identity.
-            var ticket = new AuthenticationTicket(
-                principal,
-                new AuthenticationProperties(),
-                OpenIdConnectServerDefaults.AuthenticationScheme);
+            var ticket = new AuthenticationTicket(principal, properties, OpenIdConnectServerDefaults.AuthenticationScheme);
+
+            if (!request.IsRefreshTokenGrantType())
+            {
+                // Set the list of scopes granted to the client application.
+                // Note: the offline_access scope must be granted
+                // to allow OpenIddict to return a refresh token.
+                ticket.SetScopes(AllowedRefreshTokenScopes.Intersect(request.GetScopes()));
+            }
 
             ticket.SetResources("resource-server");
 
@@ -102,6 +149,15 @@ namespace ClickerHeroesTrackerWebsite.Controllers.Api
                 {
                     OpenIdConnectConstants.Destinations.AccessToken,
                 };
+
+                // Only add the iterated claim to the id_token if the corresponding scope was granted to the client application.
+                // The other claims will only be added to the access_token, which is encrypted when using the default format.
+                if ((claim.Type == OpenIdConnectConstants.Claims.Name && ticket.HasScope(OpenIdConnectConstants.Scopes.Profile))
+                    || (claim.Type == OpenIdConnectConstants.Claims.Email && ticket.HasScope(OpenIdConnectConstants.Scopes.Email))
+                    || (claim.Type == OpenIdConnectConstants.Claims.Role && ticket.HasScope(OpenIddictConstants.Claims.Roles)))
+                {
+                    destinations.Add(OpenIdConnectConstants.Destinations.IdentityToken);
+                }
 
                 claim.SetDestinations(destinations);
             }
--- src/Website/wwwroot/js/services/authenticationService/authenticationService.spec.ts
+++ src/Website/wwwroot/js/services/authenticationService/authenticationService.spec.ts
@@ -1,26 +1,26 @@
 import { ReflectiveInjector } from "@angular/core";
-import { fakeAsync, tick } from "@angular/core/testing";
+import { fakeAsync, tick, discardPeriodicTasks } from "@angular/core/testing";
 import { BaseRequestOptions, ConnectionBackend, Http, RequestOptions } from "@angular/http";
-import { Response, ResponseOptions } from "@angular/http";
+import { Response, ResponseOptions, RequestMethod } from "@angular/http";
 import { MockBackend, MockConnection } from "@angular/http/testing";
 
-import { AuthenticationService } from "./authenticationService";
+import { AuthenticationService, IAuthTokenModel } from "./authenticationService";
 
 describe("AuthenticationService", () =>
 {
     let injector: ReflectiveInjector;
     let backend: MockBackend;
-    let lastConnection: MockConnection;
+    let lastConnection: MockConnection = null;
 
     beforeEach(() =>
     {
         injector = ReflectiveInjector.resolveAndCreate(
-        [
-            { provide: ConnectionBackend, useClass: MockBackend },
-            { provide: RequestOptions, useClass: BaseRequestOptions },
-            Http,
-            AuthenticationService,
-        ]);
+            [
+                { provide: ConnectionBackend, useClass: MockBackend },
+                { provide: RequestOptions, useClass: BaseRequestOptions },
+                Http,
+                AuthenticationService,
+            ]);
 
         spyOn(localStorage, "getItem");
         spyOn(localStorage, "setItem");
@@ -43,24 +43,83 @@ describe("AuthenticationService", () =>
             (localStorage.getItem as jasmine.Spy).and.returnValue(null);
 
             let authenticationService = injector.get(AuthenticationService) as AuthenticationService;
+
             authenticationService.isLoggedIn()
                 .subscribe(isLoggedIn => expect(isLoggedIn).toEqual(false));
             tick();
 
             expect(localStorage.getItem).toHaveBeenCalledWith("auth-tokens");
+            expect(localStorage.setItem).not.toHaveBeenCalled();
+            expect(lastConnection).toBeNull();
         }));
 
-        it("should be logged in initially when local storage is populated", fakeAsync(() =>
+        it("should be logged in initially and refresh the token when local storage is populated with a valid token", fakeAsync(() =>
         {
-            let tokens = { token_type: "someTokenType", access_token: "someAccessToken"};
+            let tokens = createCachedAuthModel();
             (localStorage.getItem as jasmine.Spy).and.returnValue(JSON.stringify(tokens));
 
             let authenticationService = injector.get(AuthenticationService) as AuthenticationService;
-            authenticationService.isLoggedIn()
-                .subscribe(isLoggedIn => expect(isLoggedIn).toEqual(true));
+
+            expect(lastConnection).not.toBeNull("no http service connection made");
+            expect(lastConnection.request.method).toEqual(RequestMethod.Post, "method invalid");
+            expect(lastConnection.request.url).toEqual("/api/auth/token", "url invalid");
+            expect(lastConnection.request.text()).toEqual("grant_type=refresh_token&refresh_token=someRefreshToken&scope=openid%20offline_access", "request body invalid");
+
+            let isLoggedIn = false;
+            authenticationService.isLoggedIn().subscribe(_ => isLoggedIn = _);
+
+            // Immediately logged in
             tick();
+            expect(isLoggedIn).toEqual(true);
+
+            let newTokens = createResponseAuthModel();
+            lastConnection.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(newTokens) })));
+            newTokens.expiration_date = new Date().getTime() + newTokens.expires_in * 1000;
+
+            // Still logged in after the token refresh
+            tick();
+            expect(isLoggedIn).toEqual(true);
 
             expect(localStorage.getItem).toHaveBeenCalledWith("auth-tokens");
+            expect(localStorage.setItem).toHaveBeenCalledWith("auth-tokens", JSON.stringify(newTokens));
+
+            // An interval was started, so abandon it.
+            discardPeriodicTasks();
+        }));
+
+        it("should be logged out initially but logged in eventually once the token is refreshed when local storage is populated with an expired token", fakeAsync(() =>
+        {
+            let tokens = createCachedAuthModel();
+            tokens.expiration_date = 0;
+            (localStorage.getItem as jasmine.Spy).and.returnValue(JSON.stringify(tokens));
+
+            let authenticationService = injector.get(AuthenticationService) as AuthenticationService;
+
+            expect(lastConnection).not.toBeNull("no http service connection made");
+            expect(lastConnection.request.method).toEqual(RequestMethod.Post, "method invalid");
+            expect(lastConnection.request.url).toEqual("/api/auth/token", "url invalid");
+            expect(lastConnection.request.text()).toEqual("grant_type=refresh_token&refresh_token=someRefreshToken&scope=openid%20offline_access", "request body invalid");
+
+            let isLoggedIn = false;
+            authenticationService.isLoggedIn().subscribe(_ => isLoggedIn = _);
+
+            // Immediately not logged in
+            tick();
+            expect(isLoggedIn).toEqual(false);
+
+            let newTokens = createResponseAuthModel();
+            lastConnection.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(newTokens) })));
+            newTokens.expiration_date = new Date().getTime() + newTokens.expires_in * 1000;
+
+            // Logged in after the token refresh
+            tick();
+            expect(isLoggedIn).toEqual(true);
+
+            expect(localStorage.getItem).toHaveBeenCalledWith("auth-tokens");
+            expect(localStorage.setItem).toHaveBeenCalledWith("auth-tokens", JSON.stringify(newTokens));
+
+            // An interval was started, so abandon it.
+            discardPeriodicTasks();
         }));
     });
 
@@ -82,7 +141,7 @@ describe("AuthenticationService", () =>
 
         it("should get auth headers when logged in", fakeAsync(() =>
         {
-            let tokens = { token_type: "someTokenType", access_token: "someAccessToken"};
+            let tokens = createCachedAuthModel();
             (localStorage.getItem as jasmine.Spy).and.returnValue(JSON.stringify(tokens));
 
             let authenticationService = injector.get(AuthenticationService) as AuthenticationService;
@@ -100,7 +159,7 @@ describe("AuthenticationService", () =>
     {
         it("should reject with an unsuccessful log in", fakeAsync(() =>
         {
-            let loggedIn = false;
+            let logInSuccessful = false;
             let error: Error;
             let isLoggedInLog: boolean[] = [];
 
@@ -111,21 +170,26 @@ describe("AuthenticationService", () =>
                     isLoggedInLog.push(isLoggedIn);
                 });
             authenticationService.logIn("someUsername", "somePassword")
-                .then(() => loggedIn = true)
+                .then(() => logInSuccessful = true)
                 .catch(e => error = e);
+
+            expect(lastConnection).not.toBeNull("no http service connection made");
+            expect(lastConnection.request.method).toEqual(RequestMethod.Post, "method invalid");
+            expect(lastConnection.request.url).toEqual("/api/auth/token", "url invalid");
+            expect(lastConnection.request.text()).toEqual("grant_type=password&username=someUsername&password=somePassword&scope=openid%20offline_access", "request body invalid");
 
             lastConnection.mockError(new Error("someError"));
             tick();
 
-            expect(loggedIn).toEqual(false);
+            expect(logInSuccessful).toEqual(false);
             expect(localStorage.setItem).not.toHaveBeenCalled();
             expect(error).toBeDefined();
             expect(isLoggedInLog).toEqual([false]);
         }));
 
-        it("should resolve with a successful log in", fakeAsync(() =>
+        it("should resolve with a successful log in and refresh the new token", fakeAsync(() =>
         {
-            let loggedIn = false;
+            let logInSuccessful = false;
             let error: Error;
             let isLoggedInLog: boolean[] = [];
 
@@ -136,17 +200,48 @@ describe("AuthenticationService", () =>
                     isLoggedInLog.push(isLoggedIn);
                 });
             authenticationService.logIn("someUsername", "somePassword")
-                .then(() => loggedIn = true)
+                .then(() => logInSuccessful = true)
                 .catch(e => error = e);
 
-            let tokens = { token_type: "someTokenType", access_token: "someAccessToken"};
-            lastConnection.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(tokens) })));
-            tick();
+            expect(lastConnection).not.toBeNull("no http service connection made");
+            expect(lastConnection.request.method).toEqual(RequestMethod.Post, "method invalid");
+            expect(lastConnection.request.url).toEqual("/api/auth/token", "url invalid");
+            expect(lastConnection.request.text()).toEqual("grant_type=password&username=someUsername&password=somePassword&scope=openid%20offline_access", "request body invalid");
 
-            expect(loggedIn).toEqual(true);
+            let tokens = createResponseAuthModel(1);
+            lastConnection.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(tokens) })));
+            lastConnection = null;
+            tokens.expiration_date = new Date().getTime() + tokens.expires_in * 1000;
+
+            // Don't tick enough to trigger the refresh interval
+            tick(1);
+
+            expect(logInSuccessful).toEqual(true);
             expect(localStorage.setItem).toHaveBeenCalledWith("auth-tokens", JSON.stringify(tokens));
             expect(error).toBeUndefined();
             expect(isLoggedInLog).toEqual([false, true]);
+
+            (localStorage.setItem as jasmine.Spy).calls.reset();
+
+            // Let the refresh interval tick
+            tick(tokens.expires_in / 2 * 1000);
+
+            expect(lastConnection).not.toBeNull("no http service connection made");
+            expect(lastConnection.request.method).toEqual(RequestMethod.Post, "method invalid");
+            expect(lastConnection.request.url).toEqual("/api/auth/token", "url invalid");
+            expect(lastConnection.request.text()).toEqual("grant_type=refresh_token&refresh_token=someNewRefreshToken1&scope=openid%20offline_access", "request body invalid");
+
+            let refreshedTokens = createResponseAuthModel(2);
+            lastConnection.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(refreshedTokens) })));
+            refreshedTokens.expiration_date = new Date().getTime() + refreshedTokens.expires_in * 1000;
+            tick();
+
+            expect(localStorage.setItem).toHaveBeenCalledWith("auth-tokens", JSON.stringify(refreshedTokens));
+            expect(error).toBeUndefined();
+            expect(isLoggedInLog).toEqual([false, true, true]);
+
+            // An interval was started, so abandon it.
+            discardPeriodicTasks();
         }));
     });
 
@@ -156,7 +251,7 @@ describe("AuthenticationService", () =>
         {
             let isLoggedInLog: boolean[] = [];
 
-            let tokens = { token_type: "someTokenType", access_token: "someAccessToken"};
+            let tokens = createCachedAuthModel();
             (localStorage.getItem as jasmine.Spy).and.returnValue(JSON.stringify(tokens));
 
             let authenticationService = injector.get(AuthenticationService) as AuthenticationService;
@@ -170,5 +265,74 @@ describe("AuthenticationService", () =>
             expect(localStorage.removeItem).toHaveBeenCalledWith("auth-tokens");
             expect(isLoggedInLog).toEqual([true, false]);
         }));
+
+        it("should stop the token refresh interval after logging out", fakeAsync(() =>
+        {
+            let isLoggedInLog: boolean[] = [];
+
+            let tokens = createCachedAuthModel();
+            (localStorage.getItem as jasmine.Spy).and.returnValue(JSON.stringify(tokens));
+
+            let authenticationService = injector.get(AuthenticationService) as AuthenticationService;
+            authenticationService.isLoggedIn()
+                .subscribe(isLoggedIn =>
+                {
+                    isLoggedInLog.push(isLoggedIn);
+                });
+
+            // Let the refresh interval tick
+            tick(tokens.expires_in / 2 * 1000);
+
+            expect(lastConnection).not.toBeNull("no http service connection made");
+            expect(lastConnection.request.method).toEqual(RequestMethod.Post, "method invalid");
+            expect(lastConnection.request.url).toEqual("/api/auth/token", "url invalid");
+            expect(lastConnection.request.text()).toEqual("grant_type=refresh_token&refresh_token=someRefreshToken&scope=openid%20offline_access", "request body invalid");
+
+            let refreshedTokens = createResponseAuthModel(2);
+            lastConnection.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(refreshedTokens) })));
+            lastConnection = null;
+            refreshedTokens.expiration_date = new Date().getTime() + refreshedTokens.expires_in * 1000;
+            tick();
+
+            expect(isLoggedInLog).toEqual([true, true]);
+            expect(localStorage.setItem).toHaveBeenCalledWith("auth-tokens", JSON.stringify(refreshedTokens));
+            (localStorage.setItem as jasmine.Spy).calls.reset();
+
+            authenticationService.logOut();
+
+            expect(localStorage.removeItem).toHaveBeenCalledWith("auth-tokens");
+            expect(isLoggedInLog).toEqual([true, true, false]);
+
+            // Let the refresh interval tick again
+            tick(tokens.expires_in / 2 * 1000);
+
+            // And nothing changed
+            expect(lastConnection).toBeNull();
+            expect(localStorage.setItem).not.toHaveBeenCalled();
+            expect(isLoggedInLog).toEqual([true, true, false]);
+        }));
     });
+
+    function createCachedAuthModel(): IAuthTokenModel
+    {
+        return {
+            token_type: "someTokenType",
+            access_token: "someAccessToken",
+            refresh_token: "someRefreshToken",
+            id_token: "someIdToken",
+            expires_in: 3600,
+            expiration_date: new Date().getTime() + 3600,
+        };
+    }
+
+    function createResponseAuthModel(num?: number): IAuthTokenModel
+    {
+        return {
+            token_type: "someNewTokenType" + (num === undefined ? "" : num.toString()),
+            access_token: "someNewAccessToken" + (num === undefined ? "" : num.toString()),
+            refresh_token: "someNewRefreshToken" + (num === undefined ? "" : num.toString()),
+            id_token: "someNewIdToken" + (num === undefined ? "" : num.toString()),
+            expires_in: 3600,
+        };
+    }
 });
