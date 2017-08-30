592d0702d65fef081b33c27ed4336861127fe00a
Elia Karagiannis <eliakaris@hotmail.com>
2017-08-30 16:32:09 -0700

    Move the logic to the app.ts component

--- WebClient/src/components/app/app.ts
+++ WebClient/src/components/app/app.ts
@@ -1,7 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app",
   templateUrl: "./app.html",
 })
-export class AppComponent { }
+export class AppComponent implements OnInit
+{
+  public ngOnInit(): void
+  {
+      if (typeof (Storage) !== "undefined") {
+        let currentTheme = localStorage.SiteThemeType;
+        let cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css";
+
+        if (currentTheme && currentTheme.toLowerCase() === "dark") {
+            cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0-alpha.6/slate/bootstrap.min.css";
+        }
+
+        document.getElementById("bootstrapStylesheet").setAttribute("href", cssUrl);
+    }
+  }
+}
--- WebClient/src/main.ts
+++ WebClient/src/main.ts
@@ -8,15 +8,4 @@ if (process.env.ENV === "prod")
     enableProdMode();
 }
 
-if (typeof (Storage) !== "undefined") {
-    let currentTheme = localStorage.SiteThemeType;
-    let cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css";
-
-    if (currentTheme && currentTheme.toLowerCase() === "dark") {
-        cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0-alpha.6/slate/bootstrap.min.css";
-    }
-
-    document.getElementById("bootstrapStylesheet").setAttribute("href", cssUrl);
-}
-
 platformBrowserDynamic().bootstrapModule(AppModule);
