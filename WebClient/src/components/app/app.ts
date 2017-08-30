ee2e77cabdd0ed5fbda9061e6e62646fd439ffa4
Elia Karagiannis <eliakaris@hotmail.com>
2017-08-30 16:38:28 -0700

    Fix default value of the css file

--- WebClient/src/components/app/app.ts
+++ WebClient/src/components/app/app.ts
@@ -8,15 +8,15 @@ export class AppComponent implements OnInit
 {
   public ngOnInit(): void
   {
-      if (typeof (Storage) !== "undefined") {
-        let currentTheme = localStorage.SiteThemeType;
-        let cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css";
+    let cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css";
+    if (typeof (Storage) !== "undefined") {
+      let currentTheme = localStorage.SiteThemeType;
 
-        if (currentTheme && currentTheme.toLowerCase() === "dark") {
-            cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0-alpha.6/slate/bootstrap.min.css";
-        }
-
-        document.getElementById("bootstrapStylesheet").setAttribute("href", cssUrl);
+      if (currentTheme && currentTheme.toLowerCase() === "dark") {
+          cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0-alpha.6/slate/bootstrap.min.css";
+      }
     }
+
+    document.getElementById("bootstrapStylesheet").setAttribute("href", cssUrl);
   }
 }
