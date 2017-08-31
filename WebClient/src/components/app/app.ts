bb99f19bbf0d125d81a128e633f315cdb23ccabe
David Federman <david.federman@outlook.com>
2017-08-30 20:08:10 -0700

    Fix lint errors (#142)

--- WebClient/src/components/app/app.ts
+++ WebClient/src/components/app/app.ts
@@ -4,19 +4,17 @@ import { Component, OnInit } from "@angular/core";
   selector: "app",
   templateUrl: "./app.html",
 })
-export class AppComponent implements OnInit
-{
-  public ngOnInit(): void
-  {
+export class AppComponent implements OnInit {
+  public ngOnInit(): void {
     let cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css";
-    if (typeof (Storage) !== "undefined") {
-      let currentTheme = localStorage.SiteThemeType;
-
+    if (localStorage) {
+      let currentTheme: string = localStorage.SiteThemeType;
       if (currentTheme && currentTheme.toLowerCase() === "dark") {
-          cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0-alpha.6/slate/bootstrap.min.css";
+        cssUrl = "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0-alpha.6/slate/bootstrap.min.css";
       }
     }
 
+    // This needs to be in the <head> so it's not part of this component's template.
     document.getElementById("bootstrapStylesheet").setAttribute("href", cssUrl);
   }
 }
