6e2d0746dee80d2c53f46bbb58cfaab357745c99
David Federman <david.federman@outlook.com>
2017-12-26 12:50:30 -0800

    Add outsider suggestions

--- WebClient/src/components/upload/upload.ts
+++ WebClient/src/components/upload/upload.ts
@@ -37,6 +37,7 @@ interface IOutsiderViewModel {
     id: number;
     name: string;
     currentLevel: Decimal;
+    suggestedLevel: Decimal;
 }
 
 interface ICalculateAscensionZoneData {
@@ -94,6 +95,7 @@ export class UploadComponent implements OnInit {
     public ascensionsLifetime: Decimal = new Decimal(0);
     public rubies: Decimal = new Decimal(0);
     public autoclickers: Decimal = new Decimal(0);
+    public remainingAncientSouls: Decimal = new Decimal(0);
 
     public calculateAscensionZoneSteps: ICalculateAscensionZoneData[];
 
@@ -113,12 +115,21 @@ export class UploadComponent implements OnInit {
         this.hydrateAncientSuggestions();
     }
 
+    public get focusBorb(): boolean {
+        return this._focusBorb;
+    }
+    public set focusBorb(value: boolean) {
+        this._focusBorb = value;
+        this.hydrateOutsiderSuggestions();
+    }
+
     // An index for quick lookup of ancient cost formulas.
     // Each formula gets the sum of the cost of the ancient from 1 to N.
     private ancientCostFormulas = this.getAncientCostFormulas();
 
     private _suggestionType = "AvailableSouls";
     private _useSoulsFromAscension = true;
+    private _focusBorb = true;
 
     private ancientsByName: { [name: string]: IAncientViewModel } = {};
     private outsidersByName: { [name: string]: IOutsiderViewModel } = {};
@@ -174,6 +185,7 @@ export class UploadComponent implements OnInit {
                 id: outsiderDefinition.id,
                 name: outsiderDefinition.name,
                 currentLevel: new Decimal(0),
+                suggestedLevel: new Decimal(0),
             };
 
             this.outsiders.push(outsider);
@@ -226,6 +238,9 @@ export class UploadComponent implements OnInit {
 
     // tslint:disable-next-line:cyclomatic-complexity
     public calculateAscensionZone(): void {
+        const latencyCounter = "AscensionZone";
+        this.appInsights.startTrackEvent(latencyCounter);
+
         // There are some circular references, so create the maps as empty first
         let upgradesMap: { [id: string]: Upgrade } = {};
         let heroesMap: { [id: string]: Hero } = {};
@@ -411,6 +426,8 @@ export class UploadComponent implements OnInit {
                 });
             }
         }
+
+        this.appInsights.stopTrackEvent(latencyCounter);
     }
 
     private handleSettings(settings: IUserSettings): void {
@@ -519,6 +536,7 @@ export class UploadComponent implements OnInit {
         this.ancientCostMultiplier = Decimal.pow(0.95, chorgorlothLevel);
 
         this.hydrateAncientSuggestions();
+        this.hydrateOutsiderSuggestions();
     }
 
     private handleError(errorMessage: string): void {
@@ -546,9 +564,184 @@ export class UploadComponent implements OnInit {
         }
     }
 
+    private hydrateOutsiderSuggestions(): void {
+        if (this.totalAncientSouls.isZero()) {
+            for (let outsider of this.outsiders) {
+                outsider.suggestedLevel = new Decimal(0);
+            }
+
+            return;
+        }
+
+        const latencyCounter = "OursiderSuggestions";
+        this.appInsights.startTrackEvent(latencyCounter);
+
+        let isMoreThan30000 = this.totalAncientSouls.greaterThan(30000);
+        let isLessThan100 = this.totalAncientSouls.lessThan(100);
+
+        let ponyLevel: Decimal;
+        let chorLevel: Decimal;
+        let phanLevel: Decimal;
+        let borbLevel: Decimal;
+        let rhageistLevel: Decimal;
+        let kariquaLevel: Decimal;
+        let orphalasLevel: Decimal;
+        let senAkhanLevel: Decimal;
+
+        // For all but Phan
+        let costToLevelFunc = (cost: Decimal) => cost.times(8).plus(1).squareRoot().minus(1).dividedBy(2).floor();
+        let levelToCostFunc = (level: Decimal) => level.plus(1).times(level).dividedBy(2);
+
+        let halfAncientSouls = this.totalAncientSouls.dividedBy(2).floor();
+
+        // Pony and Chor
+        if (isMoreThan30000) {
+            ponyLevel = new Decimal(0);
+            chorLevel = new Decimal(0);
+        } else if (isLessThan100) {
+            // Get highest buyable level for Pony
+            ponyLevel = costToLevelFunc(this.totalAncientSouls);
+
+            // If what's remaining is less than 5, go 1 less
+            if (this.totalAncientSouls.minus(levelToCostFunc(ponyLevel)).lessThan(5)) {
+                ponyLevel = ponyLevel.minus(1);
+            }
+
+            chorLevel = new Decimal(0);
+        } else {
+            // Spend at most half on Pony and Chor
+            let maxPonyChorLevel = costToLevelFunc(halfAncientSouls);
+
+            let ponyBenefitFunc = (level: Decimal) => level.times(level).times(10).plus(1);
+
+            let chorReduction = new Decimal(1).dividedBy(0.95);
+            let chorBenefitFunc = (level: Decimal) => chorReduction.pow(level);
+
+            // Pony-favored leveling
+            let ponyFavoredMaxTotalBenefit = new Decimal(0);
+            let ponyFavoredPonyLevel = new Decimal(0);
+            let ponyFavoredChorLevel = new Decimal(0);
+            ponyLevel = maxPonyChorLevel;
+            chorLevel = new Decimal(0);
+            while (ponyLevel.greaterThan(chorLevel)) {
+                let ponyCost = levelToCostFunc(ponyLevel);
+                let ponyBenefit = ponyBenefitFunc(ponyLevel);
+
+                chorLevel = Decimal.min(costToLevelFunc(halfAncientSouls.minus(ponyCost)), 150);
+                let chorBenefit = chorBenefitFunc(chorLevel);
+
+                let totalBenefit = ponyBenefit.times(chorBenefit);
+                if (totalBenefit.greaterThan(ponyFavoredMaxTotalBenefit)) {
+                    ponyFavoredMaxTotalBenefit = totalBenefit;
+                    ponyFavoredPonyLevel = ponyLevel;
+                    ponyFavoredChorLevel = chorLevel;
+                }
+
+                ponyLevel = ponyLevel.minus(1);
+            }
+
+            // Chor-favored leveling
+            let chorFavoredMaxTotalBenefit = new Decimal(0);
+            let chorFavoredPonyLevel = new Decimal(0);
+            let chorFavoredChorLevel = new Decimal(0);
+            chorLevel = Decimal.min(maxPonyChorLevel, 150);
+            ponyLevel = new Decimal(0);
+            while (chorLevel.greaterThan(ponyLevel)) {
+                let chorCost = levelToCostFunc(chorLevel);
+                let chorBenefit = chorBenefitFunc(chorLevel);
+
+                ponyLevel = costToLevelFunc(halfAncientSouls.minus(chorCost));
+                let ponyBenefit = ponyBenefitFunc(ponyLevel);
+
+                let totalBenefit = ponyBenefit.times(chorBenefit);
+                if (totalBenefit.greaterThan(chorFavoredMaxTotalBenefit)) {
+                    chorFavoredMaxTotalBenefit = totalBenefit;
+                    chorFavoredPonyLevel = ponyLevel;
+                    chorFavoredChorLevel = chorLevel;
+                }
+
+                chorLevel = chorLevel.minus(1);
+            }
+
+            // Choose the better Chor/Pony approach
+            if (ponyFavoredMaxTotalBenefit.greaterThan(chorFavoredMaxTotalBenefit)) {
+                ponyLevel = ponyFavoredPonyLevel;
+                chorLevel = ponyFavoredChorLevel;
+            } else {
+                ponyLevel = chorFavoredPonyLevel;
+                chorLevel = chorFavoredChorLevel;
+            }
+        }
+
+        if (isMoreThan30000 || isLessThan100) {
+            rhageistLevel = new Decimal(0);
+            kariquaLevel = new Decimal(0);
+            orphalasLevel = new Decimal(0);
+            senAkhanLevel = new Decimal(0);
+        }
+
+        // Borb, Phan, amd super outsiders
+        if (isLessThan100) {
+            let ponyCost = levelToCostFunc(ponyLevel);
+            let soulsMinusPonyCost = this.totalAncientSouls.minus(ponyCost);
+
+            borbLevel = soulsMinusPonyCost.greaterThan(4) ? new Decimal(2) : new Decimal(1);
+            phanLevel = soulsMinusPonyCost.minus(levelToCostFunc(borbLevel));
+        } else {
+            let borbCostCap: Decimal;
+            if (isMoreThan30000) {
+                phanLevel = new Decimal(0);
+                borbCostCap = this.totalAncientSouls.times(0.9);
+            } else {
+                phanLevel = this.totalAncientSouls.times(0.15).floor();
+
+                let superOutsiderLevel = costToLevelFunc(this.totalAncientSouls.dividedBy(20).floor());
+
+                rhageistLevel = Decimal.min(superOutsiderLevel, 50);
+                kariquaLevel = Decimal.min(superOutsiderLevel, 10);
+                orphalasLevel = Decimal.min(superOutsiderLevel, 10);
+                senAkhanLevel = Decimal.min(superOutsiderLevel, 20);
+
+                borbCostCap = this.totalAncientSouls.times(0.04);
+                if (this.focusBorb) {
+                    let extra = levelToCostFunc(superOutsiderLevel)
+                        .times(4)
+                        .minus(levelToCostFunc(rhageistLevel))
+                        .minus(levelToCostFunc(kariquaLevel))
+                        .minus(levelToCostFunc(orphalasLevel))
+                        .minus(levelToCostFunc(senAkhanLevel));
+                    borbCostCap = borbCostCap.plus(extra);
+                }
+            }
+
+            borbLevel = Decimal.min(costToLevelFunc(borbCostCap), 245);
+        }
+
+        this.outsidersByName["Chor'gorloth"].suggestedLevel = chorLevel;
+        this.outsidersByName.Ponyboy.suggestedLevel = ponyLevel;
+        this.outsidersByName.Phandoryss.suggestedLevel = phanLevel;
+        this.outsidersByName.Borb.suggestedLevel = borbLevel;
+        this.outsidersByName.Rhageist.suggestedLevel = rhageistLevel;
+        this.outsidersByName["K'Ariqua"].suggestedLevel = kariquaLevel;
+        this.outsidersByName.Orphalas.suggestedLevel = orphalasLevel;
+        this.outsidersByName["Sen-Akhan"].suggestedLevel = senAkhanLevel;
+
+        this.remainingAncientSouls = this.totalAncientSouls
+            .minus(levelToCostFunc(chorLevel))
+            .minus(levelToCostFunc(ponyLevel))
+            .minus(phanLevel)
+            .minus(levelToCostFunc(borbLevel))
+            .minus(levelToCostFunc(rhageistLevel))
+            .minus(levelToCostFunc(kariquaLevel))
+            .minus(levelToCostFunc(orphalasLevel))
+            .minus(levelToCostFunc(senAkhanLevel));
+
+        this.appInsights.stopTrackEvent(latencyCounter);
+    }
+
     private hydrateAncientSuggestions(): void {
-        const availableSoulsSuggestionsLatency = "AncientSuggestions";
-        this.appInsights.startTrackEvent(availableSoulsSuggestionsLatency);
+        const latencyCounter = "AncientSuggestions";
+        this.appInsights.startTrackEvent(latencyCounter);
 
         const baseAncient = this.playStyle === "active"
             ? "Fragsworth"
@@ -635,7 +828,7 @@ export class UploadComponent implements OnInit {
         }
 
         this.appInsights.stopTrackEvent(
-            availableSoulsSuggestionsLatency,
+            latencyCounter,
             {
                 suggestionType: this.suggestionType,
                 useSoulsFromAscension: this.useSoulsFromAscension.toString(),
