eda749b03dd35a1542db68cfbf7b14ebe99a0c0d
David Federman <david.federman@outlook.com>
2020-09-15 22:50:34 -0700

    Fix auth?

--- Website/Src/Startup.cs
+++ Website/Src/Startup.cs
@@ -29,7 +29,7 @@ namespace ClickerHeroesTrackerWebsite
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            if (this.environment.IsDevelopment() || this.environment.IsBuddy())
+            if (this.environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
